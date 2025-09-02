9bbe6b38685d63369b3f5dd194e2478dd1189fb9
fahrurrzl <fahrurrzl5798@gmail.com>
2025-09-02 21:18:11 +0700

    feat: pagination categories

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -43,8 +43,19 @@ export default {
     }
   },
   async index(req: IReqUser, res: Response) {
+    const { search, page, limit } = req.query;
     try {
       const categories = await prisma.category.findMany({
+        where: {
+          ...(search
+            ? {
+                name: {
+                  contains: search as string,
+                  mode: "insensitive",
+                },
+              }
+            : {}),
+        },
         include: {
           products: true,
           user: true,
@@ -52,11 +63,31 @@ export default {
         orderBy: {
           createdAt: "desc",
         },
+        ...(limit ? { take: Number(limit) } : {}),
+        ...(page ? { skip: (Number(page) - 1) * Number(limit) } : {}),
+      });
+
+      const total = await prisma.category.count({
+        where: {
+          ...(search
+            ? {
+                name: {
+                  contains: search as string,
+                  mode: "insensitive",
+                },
+              }
+            : {}),
+        },
       });
 
       res.status(200).json({
         message: "Categories fetched successfully",
-        data: categories,
+        data: {
+          categories,
+          totalPage: Math.ceil(total / Number(limit)),
+          currentPage: Number(page),
+          total,
+        },
       });
     } catch (error) {
       console.log(error);
